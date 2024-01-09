365365e9e18b3d1edd19d389361418fff90b7712
Ibrohim Mubarok <befousmain@gmail.com>
2024-01-09 22:21:24 +0700

    Berhasil fetch geojson dari mongo

---
+++ jscroot/controller/map/template.js
@@ -0,0 +1,6 @@
+export let xxx = "https://asia-southeast2-befous.cloudfunctions.net/Befous/AmbilDataGeojson";
+
+export let clickpopup = `
+Long : #LONG#<br>
+Lat  : #LAT#<br>
+`
--- jscroot/view/content/map.js
+++ jscroot/view/content/map.js
@@ -1,55 +1,25 @@
 import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.2/croot.js";
 //map
-import Map from 'https://cdn.skypack.dev/ol/Map.js';
-import View from 'https://cdn.skypack.dev/ol/View.js';
-import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
-import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
-import {fromLonLat} from 'https://cdn.skypack.dev/ol/proj.js';
-import Overlay from 'https://cdn.skypack.dev/ol/Overlay.js';
-import {container} from 'https://jscroot.github.io/element/croot.js';
+import {map} from '../../controller/map/config.js';
+import {onClosePopupClick,onMapPointerMove,disposePopover,onMapClick} from '../../controller/map/popup.js';
+import {onClick} from '../../controller/map/element.js';
+
+import {get} from '../../controller/map/api.js';
+import { xxx } from '../../controller/map/template.js';
+import { MakeGeojsonFromAPI, AddLayerToMAP } from '../../controller/map/controller.js';
 
 export function main(){
     setInner("biggreet","");
 }
 
-const attributions = '<a href="https://befous.github.io/" target="_blank">&copy; Befous Indonesia</a> ';
+onClick('overlay-closer',onClosePopupClick);
 
-const place = [103.61061534308766, -1.6272725906014358];
+map.on('pointermove', onMapPointerMove);
+map.on('movestart', disposePopover);
+map.on('click', onMapClick);
 
-const basemap = new TileLayer({
-    source: new OSM({
-        attributions: attributions,
-    }),
-});
-
-const defaultstartmap = new View({
-    center: fromLonLat(place),
-    zoom: 13.2,
-});
-
-const overlay = new Overlay({
-    element: container('popup'),
-    autoPan: {
-      animation: {
-        duration: 250,
-      },
-    },
-  });
-
-const popupinfo = new Overlay({
-    element: container('popupinfo'),
-    autoPan: {
-      animation: {
-        duration: 250,
-      },
-    },
-});
-
-const map = new Map({
-    layers: [
-        basemap
-    ],
-    overlays: [overlay,popupinfo],
-    target: 'map',
-    view: defaultstartmap,
+get(xxx,result => {
+    let link = MakeGeojsonFromAPI(result)
+    console.log(result)
+    AddLayerToMAP(link)
 });
